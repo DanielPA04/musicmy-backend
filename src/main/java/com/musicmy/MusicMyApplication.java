d77cae057612047ce17af90115d51bd60884189c
Daniel Perez Alonso <danielperezalonso30@gmail.com>
2024-12-24 15:38:49 +0100

    first commit

---
+++ src/main/java/com/musicmy/MusicMyApplication.java
@@ -0,0 +1,13 @@
+package com.musicmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicMyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicmy/MusicMyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicMyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
